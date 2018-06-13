afa8dde6c0441fc4efe2acc89a8e0681ce49d01b
Matt Kush <mattkush@Matts-MacBook-Pro.local>
2018-06-13 18:51:19 -0400

    concatenated JavaScript Files

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,23 @@
 var gulp = require('gulp'),
-	gutil = require('gulp-util');
+	gutil = require('gulp-util'),
+	concat = require('gulp-concat');
+
+// Paths
+var Paths = {
+    JS:    'components/js/custom/',
+    B_JS:  'components/js/bootstrap/*',
+    SCSS:  'components/sass.',
+    IMG:   'builds/development/images/',
+};
+
+var htmlSources = 'builds/development/*html';
 
 gulp.task('log', function() {
 	gutil.log('Isidore V3 is awesome');
+});
+
+gulp.task('js', function() {
+	gulp.src(Paths.JS + '*js')
+	.pipe(concat('script.js'))
+	.pipe(gulp.dest('builds/development/js'))
 });
